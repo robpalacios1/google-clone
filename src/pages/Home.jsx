a3dacab4be55e9382e40a64c822d93a450cbf65d
roberto <robpalacios1@hotmail.com>
2021-01-14 11:36:32 -0500

    add Home.jsx page

---
+++ src/pages/Home.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+/***** CSS *****/
+import './Home.css'
+
+const Home = () => {
+    return (
+        <div className="home">
+            <h1>Iam the Homepage</h1>
+            <div className="home__header">
+                <div className="home__headerLeft">
+                    {/** Link */}
+                    {/** Link */}
+                </div>
+            </div>
+
+            <div className="home__body">
+                <div className="home__headerRight">
+                    {/** Link */}
+                    {/** Link */}
+                    {/** Icon */}
+                    {/** Avatar */}
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Home
