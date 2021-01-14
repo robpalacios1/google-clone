685d1e7d96c7b1abe5943a333641faf38f9eebc6
roberto <robpalacios1@hotmail.com>
2021-01-14 11:37:08 -0500

    install material-ui/core

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React from 'react'
+
+/***** Components *****/
+import Home from './pages/Home'
+
+/***** CSS *****/
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <h1>Hello</h1>
+      {/** Home(the one with the search) */}
+      {/** SearchPage (the results page) */}
+      <Home />
     </div>
   );
 }
