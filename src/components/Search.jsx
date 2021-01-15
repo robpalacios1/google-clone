c950d3597bbbc687668eb25725e72ca6740c830d
roberto <robpalacios1@hotmail.com>
2021-01-15 14:39:03 -0500

    add useState and add onChange into input abd onClick into Button

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,27 +1,46 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { useHistory } from 'react-router-dom';
 
 /***** Icons *****/
 import SearchIcon from '@material-ui/icons/Search';
 import MicIcon from '@material-ui/icons/Mic';
-import { Button } from '@material-ui/core'
+import { Button } from '@material-ui/core';
 
 /** CSS */
 import './Search.css'
 
 const Search = () => {
+
+    const [input, setInput] = useState('')
+    const history = useHistory();
+
+    const search = (event) => {
+        event.preventDefault();
+        history.push('/search')
+    }
+
     return (
-        <div className="search">
+        <form className="search">
             <div className="search__input">
                 <SearchIcon className="search__inputIcon"/>
-                <input />
+                <input
+                    value={input}
+                    onChange={event => setInput(event.target.value)}
+                />
                 <MicIcon />
             </div>
 
             <div className="search__buttons">
-                <Button variant="outlined">Google Search</Button>
+                <Button
+                    onClick={search}
+                    type='submit'
+                    variant="outlined"
+                >
+                    Google Search
+                </Button>
                 <Button variant="outlined">I'm Feeling Lucky</Button>
             </div>
-        </div>
+        </form>
     )
 }
 
