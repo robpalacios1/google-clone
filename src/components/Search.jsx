7c065b9d8453a61678d9b9329abc45d8e0ce5eef
roberto <robpalacios1@hotmail.com>
2021-01-15 11:22:15 -0500

    add Button and import it of material-ui

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -3,6 +3,7 @@ import React from 'react'
 /***** Icons *****/
 import SearchIcon from '@material-ui/icons/Search';
 import MicIcon from '@material-ui/icons/Mic';
+import { Button } from '@material-ui/core'
 
 /** CSS */
 import './Search.css'
@@ -14,6 +15,11 @@ const Search = () => {
                 <SearchIcon className="search__inputIcon"/>
                 <input />
                 <MicIcon />
+            </div>
+
+            <div className="search__buttons">
+                <Button variant="outlined">Google Search</Button>
+                <Button variant="outlined">I'm Feeling Lucky</Button>
             </div>
         </div>
     )
