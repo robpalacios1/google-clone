037bc3850373a4fdbfbf521bf966f89e05483414
roberto <robpalacios1@hotmail.com>
2021-01-17 14:23:21 -0500

    add SearchPage.jsx component

---
+++ src/components/SearchPage.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import {useStateValue} from '../StateProvider'
+
+
+/***** CSS *****/
+import './SearchPage.css'
+
+const Searchpage = () => {
+
+    const [ {term}, dispatch] = useStateValue();
+
+    return (
+        <div className="searchPage">
+            <div className="searchPage__header">
+                <h1>{term}</h1>
+            </div>
+
+            <div className="searchPage__results">
+
+            </div>
+        </div>
+    )
+}
+
+export default Searchpage
