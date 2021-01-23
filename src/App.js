b5e8b6107563b3ef4bc60b9a65026e594d5186fe
roberto <robpalacios1@hotmail.com>
2021-01-23 13:15:16 -0500

    remove comments

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { BrowserRouter as Router, Switch, Route, } from 'react-router-dom'
 
-/***** Components *****/
+
 import Home from './components/Home'
 import SearchPage from './components/SearchPage'
 
